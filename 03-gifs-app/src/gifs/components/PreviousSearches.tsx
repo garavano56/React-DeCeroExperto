9fc4c447e2e55a983e3afc30293551da530b1d39
Alejandro <garavano56@gmail.com>
2025-09-19 00:13:52 -0300

    03-GifsApp Separacion por componentes

---
+++ 03-gifs-app/src/gifs/components/PreviousSearches.tsx
@@ -0,0 +1,27 @@
+import type { FC } from "react";
+
+interface Props {
+    searches: string[];
+}
+
+export const PreviousSearches: FC<Props> = ( { searches } ) => {
+  return (
+    <div className="previous-searches">
+        <h2>Búsquedas previas</h2>
+        <ul className="previous-searches-list">
+            <li>Goku</li>
+            <li>Saitama</li>
+            <li>Elden Ring</li>
+        </ul>
+        <ul className="previous-searches-list">
+            {   
+                searches.map( (term) => ( 
+                    <li key={ term }>
+                        { term }
+                    </li>
+                ))
+            }
+        </ul>
+    </div>
+  )
+}
