586b9e246590508e09cd5454494359d36850c67c
Alejandro <garavano54@gmail.com>
2025-09-24 16:53:03 -0300

    04-hooks

---
+++ 04-hooks-app/src/HooksApp.tsx
@@ -0,0 +1,11 @@
+
+// https://tailwindcss.com/docs/installation/using-vite
+// --> npm install tailwindcss @tailwindcss/vite
+
+export const HooksApp = () => {
+  return (
+    <div>
+        <h1>Hola Mundo</h1>
+    </div>
+  )
+}
