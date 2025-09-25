e69c44c17e6905b3afb910233d5e0b4c519aa0c5
Alejandro <garavano56@gmail.com>
2025-09-24 23:46:22 -0300

    Tailwind Css

--- 04-hooks-app/src/HooksApp.tsx
+++ 04-hooks-app/src/HooksApp.tsx
@@ -4,8 +4,8 @@
 
 export const HooksApp = () => {
   return (
-    <div>
-        <h1>Hola Mundo</h1>
+    <div className="bg-gradient">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   )
 }
