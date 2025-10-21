1a101f9c1a6d6237bcf7d3bfb126106c00bcdf98
Alejandro <garavano54@gmail.com>
2025-10-21 16:32:39 -0300

    useContext Create

---
+++ 04-hooks-app/src/09-useContext/context/UserContext.tsx
@@ -0,0 +1,47 @@
+import { useState, type PropsWithChildren, createContext } from "react"
+import type { User } from "../data/user.mock.data";
+
+// interface UserContextProps {
+//     children: React.ReactNode;
+// }
+
+type AuthStatus = 'checking' | 'authenticated' | 'not-authenticated';
+
+interface UserContextProps {
+    // state
+    authStatus: AuthStatus;
+    user: User | null;
+    // Methods
+    login: (userId: number) => boolean;
+    logout: () => void;
+}
+
+export const UserContext = createContext({} as UserContextProps);
+
+export const UserContextProvider = ({ children }: PropsWithChildren) => {
+
+    const [authStatus, setAuthStatus] = useState<AuthStatus>('checking');
+    const [user, setUser] = useState<User | null>(null);
+
+    const handleLogin = (userId: number) => {
+        console.log({ userId });
+        return true;
+    }
+
+    const handleLogout = () => {
+        console.log('logout');
+    }
+
+    return (
+        <UserContext 
+            value= {{
+                authStatus: authStatus,
+                user: user,
+                login: handleLogin,
+                logout: handleLogout
+            }}
+        >
+            { children }
+        </UserContext>
+    )
+}
