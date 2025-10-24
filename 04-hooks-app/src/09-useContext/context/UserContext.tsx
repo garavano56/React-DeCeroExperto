d330e17561bb6f65412abe7d77abca6030222bdc
Alejandro <garavano56@gmail.com>
2025-10-24 07:57:41 -0300

    UseContex uso

--- 04-hooks-app/src/09-useContext/context/UserContext.tsx
+++ 04-hooks-app/src/09-useContext/context/UserContext.tsx
@@ -1,5 +1,5 @@
 import { useState, type PropsWithChildren, createContext } from "react"
-import type { User } from "../data/user.mock.data";
+import { users, type User } from "../data/user.mock.data";
 
 // interface UserContextProps {
 //     children: React.ReactNode;
@@ -25,11 +25,23 @@ export const UserContextProvider = ({ children }: PropsWithChildren) => {
 
     const handleLogin = (userId: number) => {
         console.log({ userId });
+        const user = users.find(user => user.id === userId);
+        if (!user) {
+            console.log(`User not found ${userId}`);
+            setUser(null);
+            setAuthStatus('not-authenticated');
+            return false;
+        }
+
+        setUser(user);
+        setAuthStatus('authenticated');
         return true;
     }
 
     const handleLogout = () => {
         console.log('logout');
+        setAuthStatus('not-authenticated');
+        setUser(null);
     }
 
     return (
