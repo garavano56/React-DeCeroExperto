081b31dd9781c01aac4148e229b71e01eb34d748
Alejandro <garavano54@gmail.com>
2025-12-09 17:07:49 -0300

    Front Inicial

--- 07-teslo-shop/teslo-shop/src/shop/pages/home/HomePage.tsx
+++ 07-teslo-shop/teslo-shop/src/shop/pages/home/HomePage.tsx
@@ -1,9 +1,16 @@
-import { Button } from "@/components/ui/button"
+import { CustomPagination } from "@/components/custom/CustomPagination"
+import { products } from "@/mocks/products.mock"
+import { CustomJumbotron } from "@/shop/components/CustomJumbotron"
+import { ProductsGrid } from "@/shop/components/ProductsGrid"
 
 export const HomePage = () => {
   return (
     <>
-      
+      <CustomJumbotron title='Todos los productos' />
+
+      <ProductsGrid products={ products } />
+
+      <CustomPagination totalPages={ 7 } />
     </>
   )
 }
