0f261bbafb26f47550ba80af847153c27fae62ec
Alejandro <garavano56@gmail.com>
2025-12-12 01:11:26 -0300

    Navegacion entre pantallas

--- 07-teslo-shop/teslo-shop/src/shop/components/CustomHeader.tsx
+++ 07-teslo-shop/teslo-shop/src/shop/components/CustomHeader.tsx
@@ -1,13 +1,35 @@
 
-import { Search, ShoppingBag, Menu } from "lucide-react";
+import { Search, Menu } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { useState } from "react";
+import { useRef } from "react";
+import { Link, useParams, useSearchParams } from "react-router";
+import { cn } from "@/lib/utils";
 
 export const CustomHeader = () => {
 
-  const [cartCount] = useState(3);
-  
+  const [searchParams, setSearchParams] = useSearchParams();
+  const { gender } = useParams();
+
+  console.log( {gender});
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  const query = searchParams.get('query') || '';
+
+  const handleSearch = (event: React.KeyboardEvent<HTMLInputElement>) => {
+      if (event.key !== 'Enter') return;
+      
+      const query = inputRef.current?.value;
+      
+      const newSearchParams = new URLSearchParams();
+      if (!query) { 
+        newSearchParams.delete('query');
+      } else {
+        newSearchParams.set('query', inputRef.current!.value);
+      }
+      setSearchParams(newSearchParams);
+  }
+
   return <header className="sticky top-0 z-50 w-full border-b backdrop-blur bg-slate-50">
       <div className="container mx-auto px-4 lg:px-8">
         <div className="flex h-16 items-center justify-between">
@@ -21,18 +43,26 @@ export const CustomHeader = () => {
 
           {/* Navigation - Desktop */}
           <nav className="hidden md:flex items-center space-x-8">
-            <a href="#" className="text-sm font-medium transition-colors hover:text-primary">
-              Camisetas
-            </a>
-            <a href="#" className="text-sm font-medium transition-colors hover:text-primary">
-              Sudaderas
-            </a>
-            <a href="#" className="text-sm font-medium transition-colors hover:text-primary">
-              Chaquetas
-            </a>
-            <a href="#" className="text-sm font-medium transition-colors hover:text-primary">
-              Accesorios
-            </a>
+            <Link 
+              to="/" 
+              className={ cn(`text-sm font-medium transition-colors hover:text-primary`, 
+                              !gender ? 'underline underline-offset-4' : '') }
+            >Todos</Link>
+            <Link 
+              to="/gender/men" 
+              className={ cn(`text-sm font-medium transition-colors hover:text-primary`, 
+                              gender === 'men' ? 'underline underline-offset-4' : '') }
+            >Hombres</Link>
+            <Link 
+              to="/gender/women" 
+              className={ cn(`text-sm font-medium transition-colors hover:text-primary`, 
+                              gender === 'women' ? 'underline underline-offset-4' : '') }
+            >Mujeres</Link>
+            <Link 
+              to="/gender/kid" 
+              className={ cn(`text-sm font-medium transition-colors hover:text-primary`, 
+                              gender === 'kid' ? 'underline underline-offset-4' : '') }
+            >Niños</Link>
           </nav>
 
           {/* Search and Cart */}
@@ -40,7 +70,13 @@ export const CustomHeader = () => {
             <div className="hidden md:flex items-center space-x-2">
               <div className="relative">
                 <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
-                <Input placeholder="Buscar productos..." className="pl-9 w-64 h-9" />
+                <Input 
+                    ref={inputRef}
+                    placeholder="Buscar productos..." 
+                    className="pl-9 w-64 h-9 bg-white" 
+                    onKeyDown={ handleSearch }  
+                    defaultValue={ query }
+                />
               </div>
             </div>
             
@@ -48,12 +84,16 @@ export const CustomHeader = () => {
               <Search className="h-5 w-5" />
             </Button>
             
-            <Button variant="ghost" size="icon" className="relative">
-              <ShoppingBag className="h-5 w-5" />
-              {cartCount > 0 && <span className="absolute -top-1 -right-1 h-5 w-5 rounded-full bg-primary text-primary-foreground text-xs flex items-center justify-center">
-                  {cartCount}
-                </span>}
-            </Button>
+            <Link to="/auth/login">
+              <Button variant= 'default' size='sm' className="ml-2"
+              >Login</Button>
+            </Link>
+
+            <Link to="/admin">
+              <Button variant= 'destructive' size='sm' className="ml-2"
+              >Admin</Button>
+            </Link>
+
           </div>
         </div>
       </div>
